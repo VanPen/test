618efb985db761903e1c1e4de997431c062b12eb
VanPen <van.pen@epitech.eu>
2020-04-02 11:42:41 +0200

    first commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,34 @@
+<template>
+   <div id="test">
+   <p>{{ fullText }}</p>
+   </div>
+</template>
+<script>
+    export default {
+       el : '#test',
+       data() {
+          return {
+             hello: "CCCou",
+             word: "ouCC",
+          }
+       },
+       computed: {
+          fullText: {
+             // GETTER
+             get: function () {
+                return this.hello + ' drdrr' + this.word
+             },
+// SETTER
+             set: function (newValue) {
+                const newHelloWord= newValue.split('')
+                this.hello= newHelloWord[0]
+                this.word= newHelloWord[newHelloWord.length - 1]
+             }
+          }
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
